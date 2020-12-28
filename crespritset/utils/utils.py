2b5540521d729c3c3a3de6c468d4181515fa104d
jnt371 <76040848+jnt371@users.noreply.github.com>
2020-12-28 16:23:20 +0900

    Create utils.py

---
+++ crespritset/utils/utils.py
@@ -0,0 +1,37 @@
+import sys
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+def newAction(
+    parent,
+    text,
+    slot=None,
+    shortcut=None,
+    icon=None,
+    tip=None,
+    checkable=False,
+    enabled=True,
+    checked=False,
+):
+    """Create a new action and assign callbacks, shortcuts, etc."""
+    a = QtWidgets.QAction(text, parent)
+    if icon is not None:
+        a.setIconText(text.replace(" ", "\n"))
+        a.setIcon(newIcon(icon))
+    if shortcut is not None:
+        if isinstance(shortcut, (list, tuple)):
+            a.setShortcuts(shortcut)
+        else:
+            a.setShortcut(shortcut)
+    if tip is not None:
+        a.setToolTip(tip)
+        a.setStatusTip(tip)
+    if slot is not None:
+        a.triggered.connect(slot)
+    if checkable:
+        a.setCheckable(True)
+    a.setEnabled(enabled)
+    a.setChecked(checked)
+    return a
